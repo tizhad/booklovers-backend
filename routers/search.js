2b044a850860079a65fe13b4948290591c36c99d
tinarezanezhad <tinarezanezhaad@gmail.com>
2021-04-09 10:51:22 +0200

    Added auth middleware in fetching data

--- routers/search.js
+++ routers/search.js
@@ -1,26 +1,64 @@
 const { Router } = require("express");
 const axios = require("axios").default;
+const UserBook = require("../models").userBook;
 const Book = require("../models").book;
 const authMiddleware = require("../auth/middleware");
 
 const router = new Router();
 
-router.get("/", async (req, res) => {
+router.get("/", authMiddleware, async (req, res) => {
   const query = req.query.q;
+  const userId = req.user.id;
 
   try {
     const URL = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
     const response = await axios.get(URL);
     const googleResponse = response.data.items;
-    console.log(googleResponse.length);
     const googleEnBooks = googleResponse.filter((item) => {
       if (item.volumeInfo.language == "en") {
         return true;
       }
       return false;
     });
-    console.log(googleEnBooks.length);
-    res.status(201).send(googleEnBooks);
+    const userBooks = await UserBook.findAll({
+      where: {
+        userId: userId,
+      },
+      include: Book,
+    });
+    console.log(userBooks);
+
+    const searchResult = googleEnBooks.map((googleBook) => {
+      const filteredBooks = userBooks.filter((userBook) => {
+        if (userBook.book.googleID === googleBook.id) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      if (filteredBooks.length > 0) {
+        return {
+          googleID: googleBook.id,
+          imageURL: googleBook.volumeInfo.imageLinks.smallThumbnail,
+          title: googleBook.volumeInfo.title,
+          description: googleBook.volumeInfo.description.substring(0, 200),
+          rate: 1,
+          status: filteredBooks[0].status,
+          progress: filteredBooks[0].progress,
+        };
+      } else {
+        return {
+          googleID: googleBook.id,
+          imageURL: googleBook.volumeInfo.imageLinks.smallThumbnail,
+          title: googleBook.volumeInfo.title,
+          description: googleBook.volumeInfo.description.substring(0, 200),
+          rate: 1,
+        };
+      }
+    });
+
+    res.status(201).send(searchResult);
   } catch (error) {
     console.log(error);
     return res.status(500).send({ message: "something went wrong" });
